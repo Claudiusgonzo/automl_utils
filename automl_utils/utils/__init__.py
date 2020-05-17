da7624ddc0b4f511bbc891d2f0f6fb6f634fa50e
Neil Tenenholtz <ntenenz@users.noreply.github.com>
2020-05-17 15:27:36 -0400

    adding converter for tuple -> dict batches (#3)

---
+++ automl_utils/utils/__init__.py
@@ -0,0 +1,6 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+"""Utilities for AutoML research and development."""
+
+from .dataset import make_dict_dataset  # noqa: F401
